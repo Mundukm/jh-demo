6712492691fefd1fb4781bd9af9e431aa78070e3
Kolja <myndykmykola@gmail.com>
2017-05-11 18:49:59 +0300

    Initial commit

---
+++ src/main/java/com/myndyk/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.myndyk.config.audit;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { JhDemoSharedModule, UserRouteAccessService } from './shared';
+import { JhDemoHomeModule } from './home/home.module';
+import { JhDemoAdminModule } from './admin/admin.module';
+import { JhDemoAccountModule } from './account/account.module';
+import { JhDemoEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        JhDemoSharedModule,
+        JhDemoHomeModule,
+        JhDemoAdminModule,
+        JhDemoAccountModule,
+        JhDemoEntityModule
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class JhDemoAppModule {}
